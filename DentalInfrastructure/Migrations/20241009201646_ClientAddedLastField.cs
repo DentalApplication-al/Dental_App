785b856a270e8208f87833987ba50d0c699ec5ca
amalka1 <amarildomalka97@gmail.com>
2024-10-09 22:17:39 +0200

    Client updated database

---
+++ DentalInfrastructure/Migrations/20241009201646_ClientAddedLastField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DentalInfrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClientAddedLastField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PregnancyStatus",
+                table: "Clients",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PregnancyStatus",
+                table: "Clients");
+        }
+    }
+}
